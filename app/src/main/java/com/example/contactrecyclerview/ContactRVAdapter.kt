0ae3c07e3f0a3642fb9e8a1efca57746bc6a693a
09winnie <winfredgraceekendi@gmail.com>
2021-08-29 06:03:31 +0300

    adding an adapter of the contacts and details

---
+++ app/src/main/java/com/example/contactrecyclerview/ContactRVAdapter.kt
@@ -0,0 +1,51 @@
+package com.example.contactrecyclerview
+
+import android.content.Context
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+
+class ContactRVAdapter ( var contactsList:List<Contact>,var context: Context):RecyclerView.Adapter <ContactsViewHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactsViewHolder {
+      val itemView=LayoutInflater.from(parent.context).inflate(R.layout.contact_list,parent,false)
+        return ContactsViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: ContactsViewHolder, position: Int) {
+        var currentContact=contactsList.get(position)
+        holder.tvname.text=currentContact.name
+        holder.tvphone.text=currentContact.phoneNumber
+        holder.tvemail.text=currentContact.email
+        holder.cvcontact.setOnClickListener {
+            var intent=Intent(context,ContactDetails::class.java)
+            intent.putExtra("name",Contact.name)
+            intent.putExtra("phone",Contact.phoneNumber)
+            intent.putExtra("email",Contact.email)
+            intent.putExtra("image",Contact.imageUrl)
+            context.startActivity(intent)
+
+
+        }
+        Picasso.get().load(Contact.imageurl).resize(80,80).centerCrop().into(holder.ivcontact)
+    }
+
+    override fun getItemCount(): Int {
+      return contactsList.size
+    }
+}
+class ContactsViewHolder(itemView:View):RecyclerView.ViewHolder(itemView){
+    var tvname=itemView.findViewById<TextView>(R.id.tvname)
+    var tvphone=itemView.findViewById<TextView>(R.id.tvphone)
+    var tvemail=itemView.findViewById<TextView>(R.id.tvemail)
+    var ivcontact=itemView.findViewById<ImageView>(R.id.ivcontact)
+    var cvcontact=itemView.findViewById<CardView>(R.id.cvcontact)
+
+
+
+}
